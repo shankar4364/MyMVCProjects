142f9c8acd6ef29065de19b4e0e7216d7f1443cd
shankar4364 <shankarborade4364@gmail.com>
2025-06-19 11:49:46 +0530

    new controller added

---
+++ MVCApplication/Controllers/ShopController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApplication.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
